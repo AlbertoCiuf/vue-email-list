0758e58956c87c62bedd126bafe01085fd2494b4
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-22 16:34:25 +0100

    esercizio fatto con bonus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 const app = new Vue({
   el: '#app',
+  data: {
+    isLoading:true,
+    isError:false,  
+    mailList: [],
+    error:'ERRORE'
+  },
 
-  mounted(){},
-  
-  data: {},
+  mounted() {
+    this.isError=false;
+    this.isLoading = true;
+    for(let i=0; i<10; i++) {
+      //chiamo l'api
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      //in caso di successo:
+      .then((response) => {
+        let objData = response.data;
+        //controllo doppioni
+        if (!this.mailList.includes(objData.response)) {
+          this.mailList.push(objData.response);
+        }
+        })        
+        //in caso di errore nella chiamata:
+        .catch((error) => {
+          this.isError = true;
+        })
+      }
+    this.isLoading = false;
+      
+  },
+
+
   
 
 });
